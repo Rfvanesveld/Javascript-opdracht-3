893fc68ec84fcf4126d7de2558b14643b20337ec
Rfvanesveld <75245865+Rfvanesveld@users.noreply.github.com>
2021-04-12 13:50:16 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,29 @@
+const leeftijd = 22;
+
+if (leeftijd >= 18) { console.log
+
+("Deze man/vrouw is ouder dan 18 en mag daarom naar binnen");}
+
+else if (leeftijd < 18) { console.log
+
+("Deze man/vrouw is jonger dan 18 en mag daarom niet naar binnen!!");}
+
+const isEenVrouw = true;
+
+if (isEenVrouw){console.log 
+
+("Dit is een vrouw en heeft een ticket gekocht voor Ladies night");}
+
+else {console.log 
+
+("Dit is een man en heeft daarom geen ticket voor Ladies night");}
+
+const StatusChauffeur = true;
+
+if (StatusChauffeur){console.log 
+
+("Deze heer/mevrouw heeft niet gedronken en is de bob en mag autorijden");}
+
+else {console.log 
+
+("Deze heer/mevrouw heeft te veel gedronken en mag niet meer autorijden");}
